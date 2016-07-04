e9489b4aa7ada26b68c874d0f72121e023c47a80
James Nylen <jnylen@gmail.com>
2016-07-04 19:48:31 -0400

    Implement fetching and retrieving time series data

--- server.js
+++ server.js
@@ -1,7 +1,63 @@
-require('./lib/db').connect(function(err, db) {
+const express = require('express');
+const moment  = require('moment');
+
+const config               = require('./lib/config');
+const TemperatureDataStore = require('./lib/TemperatureDataStore');
+
+const app       = express();
+const apiRouter = express.Router();
+
+require('./lib/db').connect((err, db) => {
     if (err) throw err;
-    db.end(function(err) {
-        if (err) throw err;
-        console.log('MySQL connection closed');
+
+    const temperatureDataStore = new TemperatureDataStore(db);
+
+    apiRouter.get('/data', (req, res) => {
+        const min   = +req.query.min || +moment.utc().subtract(2, 'days');
+        const max   = +req.query.max || +moment.utc();
+        const count = +req.query.count || 500;
+
+        if (
+            moment.utc(max).diff(moment.utc(min), 'days') > 10 ||
+            count > 5000
+        ) {
+            res.json({
+                ok    : false,
+                error : 'Too much data requested (>10 days / >5000 points)'
+            });
+            return;
+        }
+
+        temperatureDataStore.fetch(min, max, count, (err, points) => {
+            if (err) {
+                res.json({
+                    ok    : false,
+                    error : err.message
+                });
+            } else {
+                res.json({
+                    ok   : true,
+                    data : points,
+                    min,
+                    max,
+                    count
+                });
+            }
+        });
+    });
+
+    app.use(config.http.basePath || '/', apiRouter);
+
+    app.listen(config.http.port, () => {
+        console.log('Listening on :' + config.http.port);
+    });
+
+    process.on('SIGINT', () => {
+        console.log();
+        db.end(err => {
+            if (err) throw err;
+            console.log('MySQL connection closed');
+            process.exit(0);
+        });
     });
 });

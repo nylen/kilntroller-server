582c9893ea099ed7ced7b25c485a385a1216a6b1
James Nylen <jnylen@gmail.com>
2017-10-05 23:16:15 -0400

    Historical data API improvements
    - Accept Moment-parsable date strings for min/max
    - Allow returning data in CSV or tab-delimited text

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const moment  = require('moment');
 const config               = require('./lib/config');
 const TemperatureDataStore = require('./lib/TemperatureDataStore');
 const UpdateListener       = require('./lib/UpdateListener');
+const utils                = require('./lib/utils');
 
 const app       = express();
 const apiRouter = express.Router();
@@ -28,8 +29,14 @@ require('./lib/db').connect((err, db) => {
     });
 
     apiRouter.get('/data', (req, res) => {
-        const min   = +req.query.min || +moment.utc().subtract(2, 'days');
-        const max   = +req.query.max || +moment.utc();
+        const min = (
+            utils.parseDate(req.query.min) ||
+            +moment.utc().subtract(2, 'days')
+        );
+        const max = (
+            utils.parseDate(req.query.max) ||
+            +moment.utc()
+        );
         const count = +req.query.count || 500;
 
         if (
@@ -49,33 +56,81 @@ require('./lib/db').connect((err, db) => {
                     ok    : false,
                     error : err.message
                 });
-            } else {
-                let actual;
-                if (points.length) {
-                    actual = {
-                        min   : points[0].timestamp,
-                        max   : points[points.length - 1].timestamp,
-                        count : points.length
-                    }
-                } else {
-                    actual = {
-                        min   : 0,
-                        max   : 0,
-                        count : 0
-                    };
-                }
-
-                res.json({
-                    ok        : true,
-                    data      : points,
-                    requested : {
-                        min,
-                        max,
-                        count
-                    },
-                    actual
-                });
+                return;
             }
+
+            let actual;
+            if (points.length) {
+                actual = {
+                    min   : points[0].timestamp,
+                    max   : points[points.length - 1].timestamp,
+                    count : points.length
+                }
+            } else {
+                actual = {
+                    min   : 0,
+                    max   : 0,
+                    count : 0
+                };
+            }
+
+            const result = {
+                ok        : true,
+                data      : points,
+                requested : {
+                    min,
+                    max,
+                    count
+                },
+                actual
+            };
+
+            const textFormats = {
+                csv : {
+                    delimiter   : ',',
+                    contentType : 'text/csv',
+                },
+                tab : {
+                    delimiter   : "\t",
+                    contentType : 'text/plain',
+                },
+            };
+            const textFormat = textFormats[req.query.format];
+
+            if (textFormat) {
+                res.status(200);
+                res.set('Content-Type', textFormat.contentType);
+
+                delete result.data;
+                res.write(JSON.stringify(result) + "\n");
+
+                res.write([
+                    'timestamp',
+                    'datetime_utc',
+                    'temperature',
+                    'rawTemp1',
+                    'rawTemp2',
+                    'rawTemp3',
+                    'setpoint',
+                ].join(textFormat.delimiter) + "\n");
+
+                points.forEach(point => {
+                    res.write([
+                        point.timestamp,
+                        utils.formatCsvDate(point.timestamp),
+                        point.temperature,
+                        point.raw.temp1,
+                        point.raw.temp2,
+                        point.raw.temp3,
+                        point.setpoint,
+                    ].join(textFormat.delimiter) + "\n");
+                });
+
+                res.end();
+                return;
+            }
+
+            res.json(result);
         });
     });
 
